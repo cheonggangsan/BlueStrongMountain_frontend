02d075d8a316dd70a886e446072b44d4f48c0611
DonghanPark <vkfkdtor00@gmail.com>
2025-11-24 17:19:58 +0900

    [BST-1] chore: add eslint config file for github actions

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
