827a5bd635578a796dd3ec9591d6587296da5cc5
DonghanPark <vkfkdtor00@gmail.com>
2025-11-24 17:45:00 +0900

    [BST-1] chore: update ESLint9 flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,22 @@
-export default {
-  languageOptions: {
-    globals: {
-      window: "readonly",
-      document: "readonly",
+import eslintRecommended from "eslint-config-eslint";
+import vueRecommended from "eslint-plugin-vue/config/recommended";
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        window: "readonly",
+        document: "readonly",
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: ["vue"],
+    rules: {
+      ...eslintRecommended.rules,
+      ...vueRecommended.rules,
     },
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {},
-};
+];
