01e45771c9f6d5d9b09768ea0b0692635fd51b1d
DonghanPark <vkfkdtor00@gmail.com>
2025-11-24 17:58:46 +0900

    [BST-1] chore: add ESLint9 rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import eslintRecommended from "eslint-config-eslint";
-import vueRecommended from "eslint-plugin-vue/config/recommended";
 
 export default [
   {
@@ -16,7 +15,12 @@ export default [
     plugins: ["vue"],
     rules: {
       ...eslintRecommended.rules,
-      ...vueRecommended.rules,
+      "vue/valid-v-for": "error", // v-for 구문이 올바른지 여부
+      "vue/valid-v-bind": "error", // v-bind의 구문이 유효한지
+      "vue/no-unused-vars": "warn", // 사용하지 않는 변수 경고
+      "vue/require-v-for-key": "error", // v-for에 key 속성이 반드시 있어야 한다는 규칙
+      "vue/no-mutating-props": "error", // prop 값 수정 금지
+      "vue/valid-v-model": "error", // v-model의 유효성 검사
     },
   },
 ];
