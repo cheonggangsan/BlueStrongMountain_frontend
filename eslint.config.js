687c7aad74fa83c6f6d5e94ddc95161688462cd2
DonghanPark <qkrehdgks11@naver.com>
2025-11-25 22:52:57 +0900

    [BST-3] chore: update eslint & prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import eslintPluginVue from "eslint-plugin-vue";
 import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
@@ -23,8 +24,15 @@ export default [
       "vue/require-v-for-key": "error",
       "vue/no-mutating-props": "error",
       "vue/valid-v-model": "error",
+
+      // "vue/html-self-closing": "off",
+      // "vue/max-attributes-per-line": "off",
+      // "vue/singleline-html-element-content-newline": "off",
+      // "vue/multiline-html-element-content-newline": "off",
     },
   },
+
+  eslintConfigPrettier,
 
   {
     files: ["*.config.*", "*.cjs"],
