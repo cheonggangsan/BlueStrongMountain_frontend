e4f8a14ac927996034d17f8eea9ccae488d4eaa6
DonghanPark <qkrehdgks11@naver.com>
2026-01-03 21:18:03 +0900

    [BSM-85] chore: add tanstack in main

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import { createApp } from "vue";
+import { VueQueryPlugin } from "@tanstack/vue-query";
+
 import App from "./App.vue";
 import "./styles/main.scss";
 import "flowbite";
 import router from "./router";
 import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
 
+import { queryClient } from "@/lib/query/queryClient";
+
 const app = createApp(App);
+
+app.use(VueQueryPlugin, { queryClient });
 app.use(router);
 app.mount("#app");
